442b7fb12144a1710cce9aba43c021bb6de3c4a5
Juliana Jux <julianalencar73@gmail.com>
2021-08-15 16:56:03 -0300

    tirei a memccpy que não precisa mais e criei a strlcpy, modificando também o makefile e libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: julianaalencar <julianaalencar@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/29 21:03:02 by julianaalen       #+#    #+#             */
-/*   Updated: 2021/08/15 16:19:13 by julianaalen      ###   ########.fr       */
+/*   Updated: 2021/08/15 16:30:55 by julianaalen      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
