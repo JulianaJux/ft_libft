e1cb803ac5345a980c756e247697e2907f41f606
Juliana Jux <julianalencar73@gmail.com>
2021-08-15 21:07:49 -0300

    norma em uma função

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: julianaalencar <julianaalencar@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/15 18:01:39 by julianaalen       #+#    #+#             */
-/*   Updated: 2021/08/15 21:04:40 by julianaalen      ###   ########.fr       */
+/*   Updated: 2021/08/15 21:07:21 by julianaalen      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_strrchr(const char *s, int c)
 	while (j >= 0)
 	{
 		if (s[j] == (char)c)
-			return ((char *)(s +j));
+			return ((char *)(s + j));
 		j--:
 	}
 	return (NULL);
