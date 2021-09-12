93cf4adac7c1c37516f57a87427dfa0b0e4f7a5b
Juliana Jux <julianalencar73@gmail.com>
2021-09-12 19:02:47 -0300

    normas

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: julianaalencar <julianaalencar@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 17:53:20 by julianaalen       #+#    #+#             */
-/*   Updated: 2021/09/12 18:56:34 by julianaalen      ###   ########.fr       */
+/*   Updated: 2021/09/12 19:02:19 by julianaalen      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 			if (!(dst->next = ft_lstnew(f(lst->next->content))))
 			{
 				ft_lstclear(&first, del);
-					return (0);
+				return (0);
 			}
 			dst = dst->next;
 		}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: julianaalencar <julianaalencar@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/29 21:03:02 by julianaalen       #+#    #+#             */
-/*   Updated: 2021/09/12 18:55:10 by julianaalen      ###   ########.fr       */
+/*   Updated: 2021/09/12 19:01:41 by julianaalen      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-typedef struct	s_list
+typedef struct s_list
 {
 	void			*content;
-	struct	s_list	*next;
+	struct s_list	*next;
 }
-				t_list;
+t_list;
 
 /*primeira parte mandatória*/
 void	*ft_memset(void *b, int c, size_t len);
