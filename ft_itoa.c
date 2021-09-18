00f7535a2a2cfee475b3531d67702d3544a14e26
Juliana Jux <julianalencar73@gmail.com>
2021-09-18 16:52:10 -0300

    itoa feita

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: julianaalencar <julianaalencar@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 10:15:55 by julianaalen       #+#    #+#             */
-/*   Updated: 2021/09/18 12:40:36 by julianaalen      ###   ########.fr       */
+/*   Updated: 2021/09/18 16:51:54 by julianaalen      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,12 @@ static	int	ft_len(long nb)
 	int	len;
 
 	len = 0;
-	if (nb < 0)
+	while (nb != 0)
 	{
-		nb = nb * -1;
+		nb /= 10;
 		len++;
 	}
-	while (nb > 0)
-	{
-		nb = nb / 10;
-		len++;
-	}
+	len ++;
 	return (len);
 }
 
@@ -36,24 +32,23 @@ char	*ft_itoa(int n)
 	long	t;
 	int		j;
 
-	t = n;
-	j = ft_len(n);
-	res = (char *)malloc(sizeof(char) * (j + 1));
-	res[j--] = '\0';
-	if (n == 0)
-	{
-		res[0] = 48;
-		return (res);
-	}
+	j = 0;
 	if (n < 0)
+		j++;
+	t = n;
+	if (t < 0)
+		t = -t;
+	j = ft_len(t);
+	res = (char *)malloc(sizeof(char) * (j + 1));
+	if (!(res))
+		return (NULL);
+	res[j] = '\0';
+	while (j-- > 0)
 	{
+		res[j] = (t % 10 + '0');
+		t = t / 10;
+	}
+	if ((res[0] == '0') && (res[1] != '\0'))
 		res[0] = '-';
-		n = n * -1;
-	}
-	while (n > 0)
-	{
-		res[j--] = 48 + (n % 10);
-		n = n / 10;
-	}
 	return (res);
 }
