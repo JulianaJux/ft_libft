7347157bdd325c04f95b3e8a13b6fc09764a13ba
Juliana Jux <julianalencar73@gmail.com>
2021-09-18 17:30:22 -0300

    norma na lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: julianaalencar <julianaalencar@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 17:53:20 by julianaalen       #+#    #+#             */
-/*   Updated: 2021/09/18 17:29:15 by julianaalen      ###   ########.fr       */
+/*   Updated: 2021/09/18 17:30:06 by julianaalen      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	if (dst == NULL)
 	{
 		ft_lstclear(&dst, del);
-			return (NULL);
+		return (NULL);
 	}
 	dst->next = ft_lstmap(lst->next, f, del);
 	return (dst);
