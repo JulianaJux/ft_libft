d55731a70cc19363145dcffb669b99b14ef13434
Juliana Jux <julianalencar73@gmail.com>
2021-09-18 15:31:49 -0300

    arrumei strim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: julianaalencar <julianaalencar@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/18 21:38:13 by julianaalen       #+#    #+#             */
-/*   Updated: 2021/09/12 18:58:43 by julianaalen      ###   ########.fr       */
+/*   Updated: 2021/09/18 15:31:15 by julianaalen      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,27 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	ini;
-	size_t	fim;
-	char	*str;
+	const char	*tmp;
+	char		*new_str;
+	size_t		len;
+	size_t		cp_len;
 
-	if (!s1 || !set)
+	if (!s1)
 		return (NULL);
-	ini = 0;
-	while (s1[ini] && ft_strchr(set, s1[ini]))
-		ini++;
-	fim = ft_strlen(s1 + ini);
-	if (fim)
-		while (s1[fim + ini - 1] != 0 && ft_strchr(set, s1[fim + ini - 1]) != 0)
-			fim--;
-	if (!(str = malloc(sizeof(char) * fim + 1)))
-		return (NULL);
-	ft_strlcpy(str, s1 + ini, fim);
-	str[fim] = '\0';
-	return (str);
+	tmp = s1;
+	while (*tmp && (*tmp == ' ' || *tmp == '\t' || *tmp == '\n'))
+		tmp++;
+	len = ft_strlen(tmp);
+	cp_len = len;
+	while ((tmp[len - 1] == ' ' || tmp[len - 1] == '\t' || tmp[len -1] == '\n')
+					&& len >= 1)
+			len--;
+	if (len == cp_len)
+			return (ft_strdup(tmp));
+	new_str = (char *)malloc(sizeof(char) * (len + 1))
+		if (!(new_str))
+			return (NULL);
+	ft_strncpy(new_str, tmp, len);
+	new_str[len] = '\0';
+	return (new_str);
 }
