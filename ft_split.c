1125716b2f4dc4a73c5ae5696927d996414d50c5
Juliana Jux <julianalencar73@gmail.com>
2021-09-18 16:08:55 -0300

    adequae split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: julianaalencar <julianaalencar@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 10:13:27 by julianaalen       #+#    #+#             */
-/*   Updated: 2021/09/18 16:05:23 by julianaalen      ###   ########.fr       */
+/*   Updated: 2021/09/18 16:08:41 by julianaalen      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,8 +70,8 @@ char	**ft_split(char const *s, char c)
 
 	j = 0;
 	t = 0;
-	spl = (char **)malloc(sizeof(char *) * (ft_word(s, c) + 1)) 
-	if (!(s || spl))
+	spl = (char **)malloc(sizeof(char *) * (ft_word(s, c) + 1))
+		if (!(s || spl))
 		return (NULL);
 	while (*s)
 	{
@@ -79,7 +79,8 @@ char	**ft_split(char const *s, char c)
 			s++;
 		if (*s != c && *s)
 		{
-			if (!(spl[j] = (char *)malloc(sizeof(char) * (ft_len(s, c) + 1))))
+			spl[j] = (char *)malloc(sizeof(char) * (ft_len(s, c) + 1));
+			if (!(spl[j])
 				return (ft_control_malloc(spl));
 			while (*s != c && *s)
 				spl[j][t++] = (char)*s++;
