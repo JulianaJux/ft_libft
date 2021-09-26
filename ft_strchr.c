99cd255c80c3dc215336a89a1f6849acff48e1b6
Juliana Jux <julianalencar73@gmail.com>
2021-09-25 23:30:07 -0300

    arrumei o strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: julianaalencar <julianaalencar@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/15 17:50:20 by julianaalen       #+#    #+#             */
-/*   Updated: 2021/09/25 17:12:36 by julianaalen      ###   ########.fr       */
+/*   Updated: 2021/09/25 23:28:28 by julianaalen      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,7 @@ char	*ft_strchr(const char *s, int c)
 			return ((char *)(s + j));
 		j++;
 	}
+	if (s[j] == (char)c)
+			return ((char *)(s + j));
 	return (NULL);
 }
