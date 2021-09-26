2f7980b5aa44735810372939ac77ff79bb1c1123
Juliana Jux <julianalencar73@gmail.com>
2021-09-25 23:33:49 -0300

    arrumei norma

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: julianaalencar <julianaalencar@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/15 17:50:20 by julianaalen       #+#    #+#             */
-/*   Updated: 2021/09/25 23:28:28 by julianaalen      ###   ########.fr       */
+/*   Updated: 2021/09/25 23:33:28 by julianaalen      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,6 @@ char	*ft_strchr(const char *s, int c)
 		j++;
 	}
 	if (s[j] == (char)c)
-			return ((char *)(s + j));
+		return ((char *)(s + j));
 	return (NULL);
 }
